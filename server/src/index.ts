0ff94f34a51daefd469012f53c591d95e9c17fa4
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-08-09 19:58:09 +0300

    implementing server

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
+// route imports
+
+
+
+// configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+// routes
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
+});
+
+
+// server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
