ce9e2f86ca275fba8d04f04e102d1f6d28148450
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-08-12 19:07:26 +0300

    removed search bar

--- client/src/app/(components)/Navbar.tsx
+++ client/src/app/(components)/Navbar.tsx
@@ -31,17 +31,6 @@ const Navbar = () => {
         >
           <Menu className="w-6 h-6" />
         </button>
-
-        <div className="relative">
-          <input
-            type="search"
-            placeholder="Search groups"
-            className="pl-10 pr-4 py-2 w-40 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
-          />
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <Bell className="text-gray-500" size={20} />
-          </div>
-        </div>
       </div>
 
       <div className="flex justify-between items-center gap-5">
