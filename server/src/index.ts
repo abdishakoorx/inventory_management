ca455584a6fdd052f4a49ffadb9b0e15dd5742a0
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-08-13 11:36:38 +0300

    setting up hosting server

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,7 @@ app.use('/users', userRoutes); // users routes
 
 
 // server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3001;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
